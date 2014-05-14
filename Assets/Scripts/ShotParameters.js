952ff786975db6bfd2e6cb4b84ab5e4e036e1e4f
Alex-Iefimenko <iefimenko.o@gmail.com>
2014-05-14 17:57:34 +0300

    Missle rotation

--- Assets/Scripts/ShotParameters.js
+++ Assets/Scripts/ShotParameters.js
@@ -1,14 +1,16 @@
 ﻿#pragma strict
 import System.Collections.Generic;
 import System.Linq;
-public var damage : int = 1;							// Damage of shot
-public var isEnemyShot : boolean = false;				// Global parameter for enemy - player recognition
-public var speed = new Vector2(10, 10);					// Speed of shot
-public var direction = new Vector2(1, 0);				// Direction of shot
+
 public var behaviourType : int = 0;						// Variable for selecting shot behaviour type:
 														// 0 - basic behaviour(fly forward)
 														// 1 - Moving to target
 														// 2 - Flack behaviour
+														
+public var damage : int = 1;							// Damage of shot
+public var isEnemyShot : boolean = false;				// Global parameter for enemy - player recognition
+public var speed = new Vector2(10, 10);					// Speed of shot
+public var direction = new Vector2(1, 0);				// Direction of shot
 
 private var activeTarget : GameObject;
 private var directionToTarget : Vector3;
@@ -25,17 +27,21 @@ function Update () {
 			if (activeTarget == null) {
 				TargetCapture();
 			} else {
-				directionToTarget = (activeTarget.transform.position.x - transform.position.x,
-				activeTarget.transform.position.y - transform.position.y,
-				0
-				).normalized;
+				directionToTarget = (activeTarget.transform.position - transform.position);
+				directionToTarget.x = 0f;
+				directionToTarget.z = 0f;	
 				lookRotation = Quaternion.LookRotation(directionToTarget);
-				transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 3);
+				transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10);
+				//transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * 10);
+				//Debug.Log(directionToTarget);
+				//Debug.Log(lookRotation);
+				
+				//Debug.Log(activeTarget.transform.position);
+				Debug.Log(transform.position);
 			}
 			
 			break;
 		case 2:
-			
 			break;
 	}
 	
@@ -57,8 +63,9 @@ function TargetCapture () {
 			}
 		}
 	}
-	activeTarget = visibleTargets[Mathf.RoundToInt(Random.Range(0, visibleTargets.Count))];
-	Debug.Log(Random.Range(0, visibleTargets.Count));
+	if (visibleTargets.Count > 0) {
+		activeTarget = visibleTargets[Mathf.RoundToInt(Random.Range(0, visibleTargets.Count))];
+	}
 }
 
 function Movement () {

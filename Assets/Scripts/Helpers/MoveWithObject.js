f6d9e4317b725bedb6c9bc4382d40955aa8e2540
Alex-Iefimenko <iefimenko.o@gmail.com>
2014-06-16 19:03:51 +0300

    follow with object script added

---
+++ Assets/Scripts/Helpers/MoveWithObject.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var target : GameObject;
+
+function Awake () {
+	if (target == null) {
+		target = GameObject.FindGameObjectWithTag("Player");
+	}
+}
+
+function Update () {
+	var place : Vector3 = Vector3(target.transform.position.x + renderer.bounds.size.x/2, 
+									target.transform.position.y, 
+									target.transform.position.z);
+	transform.position = place;
+
+}

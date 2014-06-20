00e91bf3aebd6e696159ae87cb76bbce6b050372
Alex-Iefimenko <iefimenko.o@gmail.com>
2014-06-20 13:05:23 +0300

    added autoscaling functionality

--- Assets/Scripts/GUIAndControls/SizeControlls.js
+++ Assets/Scripts/GUIAndControls/SizeControlls.js
@@ -1,5 +1,5 @@
 ﻿#pragma strict
-public var textureRatio : Vector2 = Vector2(0.5, 0.5);
+public var percentOfScreenHeight : float = 0.1;
 
 function Awake () {
 	SetScale ();
@@ -9,18 +9,9 @@ function SetScale () {
 
 	var widthHeightScreenRatio : float = (Screen.width * 1.0)/(Screen.height * 1.0);
 	var textureHeight : float = guiTexture.texture.height * 1.0;
-	var textureWidth : float = guiTexture.texture.height * 1.0;
+	var textureWidth : float = guiTexture.texture.width * 1.0;
 	var widthHeightTextureRatio : float = textureWidth / textureHeight;
-	var yScale : float = (Screen.height * 0.08) / textureHeight;
-	
-	print (Screen.width);
-	print (Screen.height);
-	
-	print (widthHeightScreenRatio);
-	print (textureHeight);
-	print (textureWidth);
-	print (widthHeightTextureRatio);
-	print (yScale);
+	var yScale : float =  percentOfScreenHeight; // textureHeight;
 	
 	this.transform.localScale = new Vector3 (yScale / widthHeightScreenRatio * widthHeightTextureRatio, yScale, 1);
 }

1965a14d1d9d021bf2205513fc47e1813ee68a73
Alex-Iefimenko <iefimenko.o@gmail.com>
2014-07-25 16:45:08 +0300

    Completed wingame / losegame screens handlers

--- Assets/Scripts/GUIAndControls/EndGame.js
+++ Assets/Scripts/GUIAndControls/EndGame.js
@@ -1,24 +1,39 @@
 ﻿#pragma strict
+import System.Collections.Generic;
+import System.Linq;
 
 public var player : GameObject;
 public var lastEnemy : GameObject;
 public var gameOverGui : GameObject;
 public var gameWinGui : GameObject;
 public var ingameGui : GameObject;
-//public var 
-function Start () {
+public var gameScore : Score;
+private var scoreCounters : List.<GUIText>;
+private var currentScore : int = 0;
 
+private var gameEnd : boolean = false;
+
+function Start () {
 }
 
 function Update () {
-	if (player == null) {
-		GameEnd(false);
-	} else if (lastEnemy == null) {
-		GameEnd(true);
+	if (gameEnd == false) {
+		if (player == null) {
+			GameEnd(false);
+		} else if (lastEnemy == null) {
+			GameEnd(true);
+		}
+	}
+	if (scoreCounters != null && scoreCounters.Count > 0) {
+		for (i in scoreCounters) {
+			if (currentScore < gameScore.GetScore()) { currentScore += 10; }
+			i.text = "0" * (5 - currentScore.ToString().length) + currentScore.ToString();;
+		}
 	}
 }
 
 function GameEnd (isWin : boolean) {
+	gameEnd = true;
 	yield WaitForSeconds (3.0);
 	Time.timeScale = 1.0;
 	Camera.main.GetComponent(Blur).enabled = true; // Blur aplied
@@ -26,7 +41,15 @@ function GameEnd (isWin : boolean) {
 	ingameGui.SetActive(false);
 	if (isWin) {
 		gameWinGui.SetActive(true);
+		StarActivation();
 	} else {
 		gameOverGui.SetActive(true);
 	}
+	scoreCounters = gameObject.GetComponentsInChildren.<GUIText>().ToList();	
+}
+
+function StarActivation () {
+	var stars : List.<Animator> = gameObject.GetComponentsInChildren.<Animator>().OrderBy(function(a){return a.name;}).ToList();
+	if ( gameScore.GetScore() * 1.0 / gameScore.GetTotalScore() * 1.0 <= 0.7 && stars[1]) { stars[1].gameObject.SetActive(false); }
+	if ( gameScore.GetScore() * 1.0 / gameScore.GetTotalScore() * 1.0 <= 0.9 && stars[2]) { stars[2].gameObject.SetActive(false); }
 }
--- Assets/Scripts/GUIAndControls/Score.js
+++ Assets/Scripts/GUIAndControls/Score.js
@@ -1,6 +1,14 @@
 ﻿#pragma strict
 
 private var score : int = 0;
+private var totalScore : int = 0;
+
+function Start () {
+	var alltargets : GameObject[] = GameObject.FindGameObjectsWithTag("Enemy");
+	for (var enemy in alltargets) {
+		totalScore += enemy.GetComponent(Health).scoreValue;
+	}
+}
 
 function Update () {
 	guiText.text = "0" * (5 - score.ToString().length) + score.ToString();
@@ -8,4 +16,12 @@ function Update () {
 
 function ScoreIncrease (newScore : int) {
 	score += newScore;
+}
+
+function GetScore () {
+	return score;
+}
+
+function GetTotalScore () {
+	return totalScore;
 }

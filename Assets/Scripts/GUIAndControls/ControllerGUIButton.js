693750389b19173341d05fd1fa07c21bbd67da3a
Alex <iefimenko.o@gmail.com>
2014-12-28 17:38:29 +0200

    Added button sounds

--- Assets/Scripts/GUIAndControls/ControllerGUIButton.js
+++ Assets/Scripts/GUIAndControls/ControllerGUIButton.js
@@ -22,10 +22,12 @@ private var isActive : boolean = true;
 
 private var blinkTime : float = 0f;
 private var blink :float = 0f;
+private var soundControll : AudioSource;
 
 function Awake (){
 	guiTextureCurrent = this.guiTexture;
 	imageNormal = guiTexture.texture;
+	soundControll = this.GetComponent.<AudioSource>();
 }
 
 function Update () {
@@ -71,6 +73,7 @@ function TouchControl(){
 					}
 					if(functionOnTouchDown != ""){
 						gameObject.SendMessage(functionOnTouchDown);
+						if (soundControll) soundControll.Play();
 					}
 					
 				}
